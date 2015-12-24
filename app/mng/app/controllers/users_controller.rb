611e09fcfc380f098aaa6582ea14fdf0ddd8f574
Yasumasa Nogami <yasumasa@hoge.com>
2015-12-25 01:56:43 +0900

    users API検索条件対応

--- app/mng/app/controllers/users_controller.rb
+++ app/mng/app/controllers/users_controller.rb
@@ -2,10 +2,13 @@ require_relative './../services/users_service'
 
 class UsersController < ApplicationController
 
+  #
+  # ユーザ一覧取得（検索）処理
   def index
     client_data = {}
+    # ユーザ検索
     user_service = UsersService.new
-    client_data[:users] = user_service.search_users
+    client_data[:users] = user_service.search_users(params)
     render :json => client_data
   end
 end
--- app/mng/app/services/users_service.rb
+++ app/mng/app/services/users_service.rb
@@ -1,6 +1,27 @@
 class UsersService
-  #
-  def search_users
-    users = User.all
+  # ユーザ検索
+  def search_users(params)
+    # バインドパラメータ
+    binds = {}
+    # sql 生成
+    sql = ""
+    sql << "SELECT * FROM users "
+    sql << "WHERE 1 = 1 "
+
+    if(params[:name])
+      sql << "and last_name || first_name like :name "
+      binds[:name] = params[:name] + "%"
+    end
+
+    if(params[:mail_address])
+      sql << "and mail_address like :mail_address "
+      binds[:mail_address] = params[:mail_address] + "%"
+    end
+
+    users = User.find_by_sql([sql, binds])
   end
+
+  # private method
+  private
+
 end
